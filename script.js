c0f49027e76b8c068e11cf8fa55a6fc710b55898
Philip Nguyen <philipnguyen953@gmail.com>
2022-08-09 12:40:55 -0700

    can no longer decrement below 0 people

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const buttonModule = (function(){
     'use strict';
 
-    //buttons
+    //buttons as public variables
     const addButton = document.querySelector("#add-people")
     const removeButton = document.querySelector("#remove-people")
 
-    //people-counter
+    //people-counter as private variables
     const _peopleCounter = document.querySelector("#people-counter");
     _peopleCounter.innerText = 0;
 
+    //private functions only accessed by buttons
     const _incrementCounter = () => _peopleCounter.innerText = ++_peopleCounter.innerText;
 
     const _decrementCounter = () => {
-
+        
         if (_peopleCounter.innerText === 0){
-            return _peopleCounter.innerText;
+            return;
 
-        }else{
+        } else if (_peopleCounter.innerText > 0){
             _peopleCounter.innerText = --_peopleCounter.innerText;
         }
-        
     };
 
+    //links public buttons to private functions
     addButton.addEventListener('click', () => _incrementCounter())
     removeButton.addEventListener('click', () => _decrementCounter())
 
